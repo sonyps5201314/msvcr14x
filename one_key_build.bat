192b6ab0443c3dd3c433e61b61776da4c4006411
sonyps5201314 <sonyps5201314@gmail.com>
2021-05-27 17:41:42 +0800

    检查CONCRT14X(D).lib不存在时才执行expand操作

--- one_key_build.bat
+++ one_key_build.bat
@@ -19,13 +19,25 @@ if "%CheckPrerequisite_Result%" NEQ "True" EXIT
 if not defined boost_ROOT (
     for /F "tokens=3* skip=2" %%P in ('reg query "HKCU\Environment" /v boost_ROOT') do @set "boost_ROOT=%%P %%Q"
 )
+
 @ECHO ON
 git clone https://github.com/sonyps5201314/ntdll.git ../ntdll
 git pull -v --progress "origin"
 git clone https://github.com/sonyps5201314/YY-Thunks.git ../YY-Thunks
 git pull -v --progress "origin"
 WScript ../ntdll/setup.vbs
-expand ./first_time_build_need_files.cab . -F:*
+
+@ECHO OFF
+FOR %%a in (Debug\CONCRT14XD.lib Release\CONCRT14X.lib x64\Debug\CONCRT14XD.lib x64\Release\CONCRT14X.lib) DO IF not exist %%a (
+    SET TO_EXPAND=TRUE
+    goto :CHECK_FOR_EXECUTE_EXPAND
+)
+:CHECK_FOR_EXECUTE_EXPAND
+if "%TO_EXPAND%" NEQ "" (
+    expand ./first_time_build_need_files.cab . -F:*
+)
+@ECHO ON
+
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\%VS_EDITION%\VC\Auxiliary\Build\vcvars32.bat"
 msbuild /m msvcr14x.sln /t:Build /p:Configuration=Debug;Platform=x86
 msbuild /m msvcr14x.sln /t:Build /p:Configuration=Release;Platform=x86
