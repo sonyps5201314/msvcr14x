3f9be6c79b916da541933eb1a88e3221146eaee8
sonyps5201314 <sonyps5201314@gmail.com>
2023-02-26 20:24:21 +0800

    Revert "Improve one_key_build.bat (#3)"
    This reverts commit 7d567cb67e23bbe0e63d1159bcf8e015be1e470f.

--- one_key_build.bat
+++ one_key_build.bat
@@ -1,36 +1,20 @@
 @ECHO OFF
-cd /d %~dp0
-GOTO find_vs
-:find_vs
-setlocal enabledelayedexpansion
-REM Visual Studio 2022
-set VSWHERE_EXE=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe
-set vsversion=17.0
-set vscount=0
-set VS_PATH=
-echo Installed visual studio instances: 
-for /f "usebackq tokens=1,* delims=: " %%i in (`"%VSWHERE_EXE%" -version %vsversion% -requires Microsoft.Component.MSBuild`) do (
-  if "%%i"=="installationPath" set /a vscount=vscount+1 && echo     !vscount!.%%j && set VS_PATH!vscount!=%%j
-)
-
-if %vscount% LSS 1 echo "The Visual Studio 2022 not installed." && goto :eof
-
-if %vscount% EQU 1 (
-  set ID=1
-) else (
-  ECHO.Which Visual Studio 2022 Edition do you want to use, default is 1:
+GOTO MENU
+:MENU
+set ID=%1
+if not defined ID (
+  ECHO.Which Visual Studio 2022 Edition do you want to use?
+  ECHO.1.Enterprise
+  ECHO.2.Professional
+  ECHO.3.Community
+  echo.Please enter the order number of your selected item:
+  
   set /p ID=
-  if not defined ID set ID=1
-  set VS_PATH=!VS_PATH%ID%!
 )
-
-set VS_PATH=!VS_PATH%ID%!
-
-if not exist "%VS_PATH%"  echo Invalid input ID. && goto :eof
-
-for /F "delims=" %%i in ("%VS_PATH%") do set VS_EDITION=%%~ni
-
-:start_build
+if "%id%"=="1" SET VS_EDITION=Enterprise
+if "%id%"=="2" SET VS_EDITION=Professional
+if "%id%"=="3" SET VS_EDITION=Community
+if not defined VS_EDITION EXIT
 
 WScript check_prerequisite.vbs
 set /P CheckPrerequisite_Result=<CheckPrerequisite_Result.txt
@@ -60,7 +44,7 @@ WScript ../ntdll/setup.vbs
 
 @ECHO ON
 
-CALL "%VS_PATH%\VC\Auxiliary\Build\vcvars32.bat"
+CALL "C:\Program Files\Microsoft Visual Studio\2022\%VS_EDITION%\VC\Auxiliary\Build\vcvars32.bat"
 msbuild "../YY-Thunks\src\YY-Thunks.UnitTest\YY-Thunks.UnitTest.vcxproj" -t:Build_YY_Thunks_List_hpp
 
 msbuild /m msvcr14x.sln /t:Build /p:Configuration=Debug;Platform=x86
