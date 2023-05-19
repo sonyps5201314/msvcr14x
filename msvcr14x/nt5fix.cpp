ce199dc0d4d0a16ae0d092aafb8ae6fb64fdcc2a
sonyps5201314 <sonyps5201314@gmail.com>
2023-05-19 11:38:01 +0800

    适配最新的外部依赖

--- msvcr14x/nt5fix.cpp
+++ msvcr14x/nt5fix.cpp
@@ -1,87 +1,6 @@
 #include "stdafx.h"
 
-//#include "awint.hpp"
-#include <internal_shared.h>
-
-// 下面的代码修改自老版本的awint.hpp
-// Use this macro for caching a function pointer from a DLL
-#define STOREFUNCTIONPOINTER(instance, function_name) \
-    __KERNEL32Functions[e##function_name] = reinterpret_cast<PVOID>(GetProcAddress(instance, #function_name));
-
-// Use this macro as a cached function pointer from a DLL
-#define DYNAMICGETCACHEDFUNCTION(function_pointer_type, function_name, variable_name) \
-    const auto variable_name = reinterpret_cast<function_pointer_type>(__KERNEL32Functions[e##function_name])
-
-#define IFDYNAMICGETCACHEDFUNCTION(function_pointer_type, function_name, variable_name) \
-    DYNAMICGETCACHEDFUNCTION(function_pointer_type, function_name, variable_name);      \
-    if (variable_name != nullptr)
-
-enum wrapKERNEL32Functions {
-	eCompareStringA,
-	eCompareStringW,
-	eMaxKernel32Function
-};
-static PVOID __KERNEL32Functions[eMaxKernel32Function] = { nullptr };
-
-using PFNCOMPARESTRINGA = int(WINAPI*)(__in LCID Locale, __in DWORD dwCmpFlags, __in_ecount(cchCount1) PCNZCH lpString1, __in int cchCount1, __in_ecount(cchCount2) PCNZCH lpString2, __in int cchCount2);
-using PFNCOMPARESTRINGW = int(WINAPI*)(__in LCID Locale, __in DWORD dwCmpFlags, __in_ecount(cchCount1) PCNZWCH lpString1, __in int cchCount1, __in_ecount(cchCount2) PCNZWCH lpString2, __in int cchCount2);
-
-// 下面的代码来自老版本的winapisupp.cpp
-
 #ifndef _WIN64
-extern "C"
-int
-WINAPI
-CompareStringA(
-	_In_ LCID     Locale,
-	_In_ DWORD    dwCmpFlags,
-	_In_reads_(cchCount1) PCNZCH lpString1,
-	_In_ int      cchCount1,
-	_In_reads_(cchCount2) PCNZCH  lpString2,
-	_In_ int      cchCount2)
-{
-	IFDYNAMICGETCACHEDFUNCTION(PFNCOMPARESTRINGA, CompareStringA, pfCompareStringA) {
-		if (Locale == LOCALE_INVARIANT)
-		{
-			if (GetWindowsVersionEx(FALSE) <= 0x0500)
-			{
-				Locale = MAKELCID(MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US), SORT_DEFAULT);
-			}
-		}
-		return pfCompareStringA(Locale, dwCmpFlags, lpString1, cchCount1, lpString2, cchCount2);
-	}
-
-	DebugBreak();
-	return 0;
-}
-
-extern "C"
-int
-WINAPI
-CompareStringW(
-	_In_ LCID Locale,
-	_In_ DWORD dwCmpFlags,
-	_In_NLS_string_(cchCount1) PCNZWCH lpString1,
-	_In_ int cchCount1,
-	_In_NLS_string_(cchCount2) PCNZWCH lpString2,
-	_In_ int cchCount2
-)
-{
-	IFDYNAMICGETCACHEDFUNCTION(PFNCOMPARESTRINGW, CompareStringW, pfCompareStringW) {
-		if (Locale == LOCALE_INVARIANT)
-		{
-			if (GetWindowsVersionEx(FALSE) <= 0x0500)
-			{
-				Locale = MAKELCID(MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US), SORT_DEFAULT);
-			}
-		}
-		return pfCompareStringW(Locale, dwCmpFlags, lpString1, cchCount1, lpString2, cchCount2);
-	}
-
-	DebugBreak();
-	return 0;
-}
-
 #define CALINFO_MAX_YEAR 2029
 
 #define LL2FILETIME( ll, pft )\
@@ -352,20 +271,3 @@ BOOL WINAPI TzSpecificLocalTimeToSystemTime(
 		return FileTimeToSystemTime(&ft, lpUniversalTime);
 }
 #endif
-
-static int __cdecl initialize_pointers() {
-	HINSTANCE hKernel32 = GetModuleHandleW(L"kernel32.dll");
-	_Analysis_assume_(hKernel32);
-
-	STOREFUNCTIONPOINTER(hKernel32, CompareStringA);
-	STOREFUNCTIONPOINTER(hKernel32, CompareStringW);
-
-	return 0;
-}
-
-#ifndef _WIN64
-#pragma comment(linker, "/include:?force_included__nt5fix_pinit@@3P6AHXZA")
-#else
-#pragma comment(linker, "/include:?force_included__nt5fix_pinit@@3P6AHXZEA")
-#endif
-_CRTALLOC(".CRT$XIC") /*static*/ _PIFV force_included__nt5fix_pinit = initialize_pointers;
