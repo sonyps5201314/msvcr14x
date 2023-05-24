cb9793ec19b82eb1404769325e9997014442d38d
sonyps5201314 <sonyps5201314@gmail.com>
2023-05-24 21:04:43 +0800

    修正nmake使用脚本

--- msvcr14x_nmake.cmd
+++ msvcr14x_nmake.cmd
@@ -1,3 +1,8 @@
 @echo off
-if %Platform% == x64 set LIB=%msvcr14x_ROOT%\x64\Release;%msvcr14x_ROOT%\x64\Debug;%msvcr14x_ROOT%\x64\AnsiRelease;%msvcr14x_ROOT%\x64\AnsiDebug;%LIB%
-if %Platform% == x86 set LIB=%msvcr14x_ROOT%\Release;%msvcr14x_ROOT%\Debug;%msvcr14x_ROOT%\AnsiRelease;%msvcr14x_ROOT%\AnsiDebug;%LIB%
+if not defined Platform set Platform=%VSCMD_ARG_TGT_ARCH%
+if not defined Platform (
+	echo The environment variable [Platform] is not set!
+	exit
+	)
+if "%Platform%" == "x64" set LIB=%msvcr14x_ROOT%\x64\Release;%msvcr14x_ROOT%\x64\Debug;%msvcr14x_ROOT%\x64\AnsiRelease;%msvcr14x_ROOT%\x64\AnsiDebug;%LIB%
+if "%Platform%" == "x86" set LIB=%msvcr14x_ROOT%\Release;%msvcr14x_ROOT%\Debug;%msvcr14x_ROOT%\AnsiRelease;%msvcr14x_ROOT%\AnsiDebug;%LIB%
