3e06d7d6ace6f72ffcff50c2719e5804cd7943cd
sonyps5201314 <sonyps5201314@gmail.com>
2024-06-18 16:48:06 +0800

    适配最新的外部依赖

--- mfc14x/mfc14x.cpp
+++ mfc14x/mfc14x.cpp
@@ -13,31 +13,10 @@
 #pragma comment(lib,"win2k3_kernl32p.lib")
 #endif
 
-#ifndef _WIN64
-#define DllMain DllMain_Existence
-#include "..\atlmfc\src\mfc\dllinit.cpp"
-#undef DllMain
+#pragma comment(linker, "/ENTRY:DllMainCRTStartupForYY_Thunks")
 extern "C"
 {
-	//导出一个外部弱符号，指示当前是否处于强行卸载模式。
+	const void* __pfnDllMainCRTStartupForYY_Thunks;
 	BOOL __YY_Thunks_Process_Terminating;
-	BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
-	{
-		switch (dwReason)
-		{
-		case DLL_PROCESS_DETACH:
-			//我们可以通过 lpReserved != NULL 判断，当前是否处于强行卸载模式。
-			__YY_Thunks_Process_Terminating = lpReserved != NULL;
-			break;
-		}
-		return DllMain_Existence(hInstance, dwReason, lpReserved);
-	}
-}
-#else
-#include "..\atlmfc\src\mfc\dllinit.cpp"
-#endif
-
-extern "C"
-{
 	const void* __acrt_atexit_table;
 }
--- msvcr14x/msvcr14x.cpp
+++ msvcr14x/msvcr14x.cpp
@@ -12,28 +12,10 @@
 #pragma comment(lib,"win2k3_kernl32p.lib")
 #endif
 
-#pragma comment(linker, "/ENTRY:__acrt_DllMain")
-
-#ifndef _WIN64
-#define __acrt_DllMain DllMain_Existence
-#include "..\..\ucrt\dll\appcrt_dllmain.cpp"
-#undef __acrt_DllMain
+#pragma comment(linker, "/ENTRY:DllMainCRTStartupForYY_Thunks")
 extern "C"
 {
-	//导出一个外部弱符号，指示当前是否处于强行卸载模式。
+	BOOL WINAPI __acrt_DllMain(_In_ HINSTANCE hInstance, _In_ DWORD fdwReason, _In_ LPVOID lpvReserved);
+	const void* __pfnDllMainCRTStartupForYY_Thunks = __acrt_DllMain;
 	BOOL __YY_Thunks_Process_Terminating;
-	BOOL WINAPI __acrt_DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
-	{
-		switch (dwReason)
-		{
-		case DLL_PROCESS_DETACH:
-			//我们可以通过 lpReserved != NULL 判断，当前是否处于强行卸载模式。
-			__YY_Thunks_Process_Terminating = lpReserved != NULL;
-			break;
-		}
-		return DllMain_Existence(hInstance, dwReason, lpReserved);
-	}
 }
-#else
-#include "..\..\ucrt\dll\appcrt_dllmain.cpp"
-#endif
