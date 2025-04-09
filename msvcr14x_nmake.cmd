b916317154afdf9aa57c2bea91d029f8d27a1caa
sonyps5201314 <sonyps5201314@gmail.com>
2025-04-09 11:07:41 +0800

    清理脚本配置中的无效项

--- msvcr14x_nmake.cmd
+++ msvcr14x_nmake.cmd
@@ -4,5 +4,5 @@ if not defined Platform (
 	echo The environment variable [Platform] is not set!
 	exit
 	)
-if "%Platform%" == "x64" set LIB=%msvcr14x_ROOT%\x64\Release;%msvcr14x_ROOT%\x64\Debug;%msvcr14x_ROOT%\x64\AnsiRelease;%msvcr14x_ROOT%\x64\AnsiDebug;%LIB%
-if "%Platform%" == "x86" set LIB=%msvcr14x_ROOT%\Release;%msvcr14x_ROOT%\Debug;%msvcr14x_ROOT%\AnsiRelease;%msvcr14x_ROOT%\AnsiDebug;%LIB%
+if "%Platform%" == "x64" set LIB=%msvcr14x_ROOT%\x64\Release;%msvcr14x_ROOT%\x64\Debug;%LIB%
+if "%Platform%" == "x86" set LIB=%msvcr14x_ROOT%\Release;%msvcr14x_ROOT%\Debug;%LIB%
--- msvcr14x_nmake.ps1
+++ msvcr14x_nmake.ps1
@@ -1,6 +1,6 @@
 if ($env:Platform -eq 'x64') {
-    $env:LIB = "$env:msvcr14x_ROOT\x64\Release;$env:msvcr14x_ROOT\x64\Debug;$env:msvcr14x_ROOT\x64\AnsiRelease;$env:msvcr14x_ROOT\x64\AnsiDebug;$env:LIB"
+    $env:LIB = "$env:msvcr14x_ROOT\x64\Release;$env:msvcr14x_ROOT\x64\Debug;$env:LIB"
 }
 elseif($env:Platform -eq 'x86') {
-    $env:LIB = "$env:msvcr14x_ROOT\Release;$env:msvcr14x_ROOT\Debug;$env:msvcr14x_ROOT\AnsiRelease;$env:msvcr14x_ROOT\AnsiDebug;$env:LIB"
+    $env:LIB = "$env:msvcr14x_ROOT\Release;$env:msvcr14x_ROOT\Debug;$env:LIB"
 }
